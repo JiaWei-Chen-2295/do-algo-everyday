35f951a00685ee2204b20d5751ed676762e46559
JiaWei-Chen-2295 <81863760+JiaWei-Chen-2295@users.noreply.github.com>
2024-04-22 18:49:13 +0800

    Update orginalCode.java

--- orginalCode.java
+++ orginalCode.java
@@ -1,37 +1,31 @@
 class Solution {
     public int myAtoi(String s) {
         int n = s.length();
-        // 通过 start 变量来跳过前面的空格
-        int start = 0; 
+        int start = 0;
         boolean isNegative = false;
-        StringBuffer sb = new StringBuffer();
 
         // Skip leading whitespace
-        while (start < n &&  s.charAt(start) == ' ') {
+        while (start < n && Character.isWhitespace(s.charAt(start))) {
             start++;
         }
 
-        // 判断结果符号
         if (start < n && (s.charAt(start) == '+' || s.charAt(start) == '-')) {
             isNegative = s.charAt(start) == '-';
             start++;
         }
 
+        // Convert digits to integer
         long result = 0;
-        for (int i = 0;i < start;i++) {
-            char temp = s.charAt(i); 
-           // if (temp == '-') {isNegtive = true; }
+        for (int i = start; i < n; i++) {
+            char temp = s.charAt(i);
             if (temp >= '0' && temp <= '9') {
                 result = result * 10 + (temp - '0');
-
                 if (result > Integer.MAX_VALUE) {
                     break;  // Early exit on overflow
                 }
-                
             } else {
                 break;  // Non-digit character encountered, stop processing
-            }       
-            
+            }
         }
 
         // Apply sign and handle overflow
@@ -45,6 +39,6 @@ class Solution {
             return Integer.MAX_VALUE;
         }
 
-        return (int)result;
+        return (int) result;
     }
 }
